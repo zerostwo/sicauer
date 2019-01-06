97f8a92c631867da0a5e94f83a333f53c30e02bc
zerostwo <zerostwo@163.com>
2019-01-06 22:48:05 +0800

    update

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'Sinclair Duan',
+        'title': 'Bolg',
+        'content': 'Lalala',
+        'date_post': 'May 24, 2019'
+    },
+    {
+        'author': 'Sinclair Li',
+        'title': 'Blog 2',
+        'content': 'What?',
+        'date_post': 'May 23, 2019'
+    }
+]
+
 @app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
+@app.route("/home/")
+def home():
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/about/')
 def about():
-    return 'About Page!'
+    return render_template('about.html', title='about')
 
 
 if __name__ == '__main__':
