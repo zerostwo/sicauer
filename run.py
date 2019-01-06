44538a2bbefc8ccee43d1a3df929a028d542d555
zerostwo <zerostwo@163.com>
2019-01-06 17:08:05 +0800

    update

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
+# run.py
 from flask import Flask
-from flask_script import Manager
-
 
 app = Flask(__name__)
-manager = Manager(app)
+
 
 @app.route('/')
 def index():
-    return "Index page"
+    return '<h1>Hello World!</h1>'
 
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
