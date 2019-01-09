867848863df295e9f56768d4b099b31e10bc7c47
zerostwo <zerostwo@163.com>
2019-01-09 12:07:44 +0800

    update

--- app/routes.py
+++ app/routes.py
@@ -30,7 +30,7 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(f'Account created for {form.username.data}! You are now able to log in', 'success')
+        flash('Account created for {form.username.data}! You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
