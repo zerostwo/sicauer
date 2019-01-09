c9d07ade73d330c144616b17cde51c2e96658f77
zerostwo <zerostwo@163.com>
2019-01-10 01:01:51 +0800

    update

--- app/users/routes.py
+++ app/users/routes.py
@@ -70,7 +70,7 @@ def user_posts(username):
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
     posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('users.user_posts.html', posts=posts, user=user)
+    return render_template('user_posts.html', posts=posts, user=user)
 
 
 @users.route("/reset_password/", methods=['GET', "POST"])
