7fd213c9836eda5c93049f6b7b18b07081282fb9
zerostwo <zerostwo@163.com>
2019-01-18 11:40:53 +0800

    update

--- app/books/routes.py
+++ app/books/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from app.books.forms import Book
 from app.books.utils import is_isbn_or_key
 
 books = Blueprint('books', __name__)
@@ -6,5 +8,9 @@ books = Blueprint('books', __name__)
 
 @books.route('/book/search/<q>/<page>')
 def search(q, page):
-    is_isbn_or_key(q)
-    pass
+    isbn_or_key = is_isbn_or_key(q)
+    if isbn_or_key == 'isbn':
+        result = Book.search_by_isbn(q)
+    else:
+        result = Book.search_by_keyword(q)
+    return jsonify(result)
