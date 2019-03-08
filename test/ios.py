f57ddd26c24a9dac761ac20b1a0ee3e982c9b51a
zerostwo <zerostwo@163.com>
2019-03-08 16:09:18 +0800

    isicau

--- test/ios.py
+++ test/ios.py
@@ -1,5 +1,36 @@
 import requests
+from lxml import etree
+import datetime as d
+from bs4 import BeautifulSoup
 
-url = 'http://ios.scdxc.app/'
-print(requests.get(url).text)
 
+def student_name(student_id, pwd):
+    session = requests.Session()
+    t = d.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        index = session.get('http://jiaowu.sicau.edu.cn/web/web/web/index.asp', timeout=5)
+        index.encoding = 'gb2312'
+        seletor = etree.HTML(index.text)
+        sign = seletor.xpath("//input[@name='sign']/@value")
+        data = {'user': student_id, 'pwd': pwd, 'lb': 'S', 'submit': '', 'sign': sign}
+        try:
+            post_url = 'http://jiaowu.sicau.edu.cn/jiaoshi/bangong/check.asp'
+            try:
+                session.post(post_url, data=data, timeout=5)
+                data = session.get('http://jiaowu.sicau.edu.cn/xuesheng/cx/zhcp/bzr_zhcj.asp', timeout=5)
+            except:
+                print(student_id, pwd, 'Connection_timed_out_2')
+            data.encoding = 'gb2312'
+            soup = BeautifulSoup(data.text)
+            nameAndId = soup.find_all('td', {"class": "g_body"})
+            #             for i in range(int(len(nameAndId))):
+            #                 print(nameAndId[i*3+4].string, nameAndId[i*3+5].string)
+            return nameAndId, len(nameAndId)
+        except:
+            print(student_id, pwd, 'wrong_password', t)
+    except:
+        print(student_id, pwd, 'Connection_timed_out_1', t)
+
+
+if __name__ == '__main__':
+    print(student_name(201702420, 981211))
